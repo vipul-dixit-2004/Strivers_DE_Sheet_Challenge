4b28ec98cc5aacacff24733e21d99f5ec5e4b150
Vipul Dixit <72184734+vipul-dixit-2004@users.noreply.github.com>
2023-08-26 14:29:35 +0530

    Create gcd.java

---
+++ Maths_and_stuff/gcd.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public static int calcGCD(int n, int m){
+        int gcd = 1;
+        int small = n>m?m:n;
+        for(int i=small;i>=2;i--){
+            if(n%i==0 && m%i==0){
+                gcd = i;
+            }
+        }
+        return gcd;
+    }
+    public static int gcdRecursion(int n,int m){
+      while(n>0 && m>0){
+            if(n>m) n=n%m;
+            else m = m%n;
+        }
+        if(n==0) return m;
+        return n;
+    }
+}
